8d17d6256c480ecdfce62707bc11c727a924c879
vincewangto <heng.w214@gmail.com>
2020-12-27 10:43:01 -0500

    Init setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div>
+          {/* <NavBar /> */}
+          {/* <Route path="/" exact component={Login} />
+                 <Route path="/home" exact component={requireAuth(Home)} /> */}
+          Hello World
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
